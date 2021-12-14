0f174af36f8797db3e8a4088f4f9530f65552497
AndressaStefany <astefanysoliveira@gmail.com>
2021-12-14 18:12:22 -0500

    Library mode

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='emergentlanguages',
+    version='0.1',
+    url='https://github.com/laurestine/nlp-emergent-languages',
+    author='Andressa Stéfany Oliveira, Hugo Sonnery, Laurestine Bradford',
+    author_email='astefanysoliveira@gmail.com, sonneryhugo42370@gmail.com, laurestineib@gmail.com',
+    description='Compositionality as robustness: towards realistic emergent languages over a noisy communication channel',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
